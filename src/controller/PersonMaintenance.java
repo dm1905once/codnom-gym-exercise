4f5ebca85f84bbb96b1b2fe75403a5ea22943046
danielm <dm1905once@gmail.com>
2026-04-01 20:02:33 -0700

    Last minor updates to the load from CSV

--- src/controller/PersonMaintenance.java
+++ src/controller/PersonMaintenance.java
@@ -7,6 +7,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PersonMaintenance {
 
@@ -24,36 +26,53 @@ public class PersonMaintenance {
 
     public static void initialPersonLoad(String sourceFilePath) throws IOException{
         try (BufferedReader bfr = new BufferedReader(new FileReader(sourceFilePath))) {
-            String record = bfr.readLine(); // Ignore first line
+            // Ignore first line because it contains headers
+            String record = bfr.readLine();
 
             while ((record = bfr.readLine()) != null) {
-                String[] fields = record.split(",", -1);
+                List<String> fields = Arrays.stream(record.split(",", -1))
+                        .map(String::trim)
+                        .toList();
 
-                if (fields[0].isEmpty() || fields[1].isEmpty() || fields[2].isEmpty() || fields[3].isEmpty()) {
-                    throw (new IOException());
+                String authLevel = fields.get(0);
+                String nameFirst = fields.get(1);
+                String nameLast = fields.get(2);
+                String DOB = fields.get(3);
+
+                if (!authLevel.isEmpty()  && !authLevel.substring(0,1).startsWith("#")){
+
+                    if (nameFirst.isEmpty() || nameLast.isEmpty() || DOB.isEmpty()) {
+                        throw (new IOException());
+                    }
+
+                    System.out.println("Creating person ...");
+                    PersonBuilder builder;
+                    builder = new PersonBuilder(authLevel, nameFirst, nameLast, DOB);
+
+                    // Optional parameters:
+                    String address1 = fields.get(4);
+                    String address2 = fields.get(5);
+                    String addressCity = fields.get(6);
+                    String dateJoined = fields.get(7);
+
+                    //Optional.ofNullable(fields.get(4)).ifPresent(builder::setAddress1);
+                    if (!address1.isEmpty()) {
+                        builder.setAddress1(address1);
+                    }
+                    if (!address2.isEmpty()) {
+                        builder.setAddress2(address2);}
+
+                    if (!addressCity.isEmpty()) {
+                        builder.setAddressCity(addressCity);
+                    }
+                    if (!dateJoined.isEmpty()) {
+                        builder.setDateJoined(dateJoined);}
+
+                    Person alguien = builder.build();
+                    System.out.println(alguien.toString());
                 }
 
-                System.out.println("Creating person ...");
-                PersonBuilder builder;
-                builder = new PersonBuilder(fields[0], fields[1], fields[2], fields[3]);
 
-                // Optional parameters:
-                //Optional.ofNullable(fields[4]).ifPresent(builder::setAddress1);
-                if (!fields[4].isEmpty()) {
-                    builder.setAddress1(fields[4]);
-                }
-                if (!fields[5].isEmpty()) {
-                    builder.setAddress2(fields[5]);
-                }
-                if (!fields[6].isEmpty()) {
-                    builder.setAddressCity(fields[6]);
-                }
-                if (!fields[7].isEmpty()) {
-                    builder.setDateJoined(fields[7]);
-                }
-
-                Person alguien = builder.build();
-                System.out.println(alguien.toString());
 
             }
         }
